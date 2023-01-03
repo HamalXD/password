2a7df66ea0c5a9bb9a8b273905bfca4a21c1a3fb
Hamal <117851589+HamalXD@users.noreply.github.com>
2023-01-03 15:09:07 +0545

    hide/show-password

---
+++ App.js
@@ -0,0 +1,58 @@
+// import './App.css';
+// // import ComponentLifeCycles from './ComponentLifeCycles'
+// // import ComponentUnmounting from './ComponentUnmounting';
+// import TimerCounterApp from './TimerCounterApp';
+// import List from './List';
+
+// function App() {
+//   // let name = "Ruby";
+//   return (
+//       <div className="App">
+//           {/* <ComponentUnmounting/>
+//           <ComponentLifeCycles name1 = {name} /> */}
+//           <TimerCounterApp/><br></br>
+//           <counter></counter>
+//           <List/>
+//       </div>
+//   )
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react'
+// import ListDisplay from './ListDisplay'
+// import { Fruits } from './Fruits'
+// import RegistrationForms from './RegistrationForms'
+// import Forms from './Forms'
+import HidePassword from './HidePassword'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Fruits/> */}
+        {/* <ListDisplay/> */}
+        {/* <RegistrationForms/> */}
+        {/* <Forms/> */}
+        <HidePassword/>
+      </div>
+    )
+  }
+}
+
+
+// import React from 'react'
+// import Authorized from './authorized'
+
+// function App() {
+//   return (
+//     <div>
+//       <Authorized/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
---
+++ HidePassword.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react';
+import { Icon } from 'react-icons-kit'
+import {eye} from 'react-icons-kit/feather/eye'
+import {eyeOff} from 'react-icons-kit/feather/eyeOff'
+
+function HidePassword() {
+    const [type , setType] = useState('password');
+    const [icon, setIcon] = useState(eyeOff);
+
+    const handleToggle = () => {
+        if(type === 'password') {
+            setIcon(eye);
+            setType('text');
+        }
+        else{
+            setIcon(eyeOff);
+            setType('password');
+        }
+}   
+  return (
+    <div className='wrapper'>
+        <div className='input-field'>
+            <input type= {type}/>
+            <span onClick={handleToggle}><Icon icon={icon} size = {25}/></span>
+        </div>
+    </div>
+  )
+}
+
+export default HidePassword;
